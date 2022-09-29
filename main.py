8988ec80053aee09823d575a1400a015abecd6df
bgonzalez <bgonzalez@vivelibre.es>
2022-09-29 10:42:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils.app_utils import *
+
+
+if __name__ == '__main__':
+    data_travel = get_travel_df()
+    get_input_file(data_travel)
+
+
---
+++ utils/app_utils.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import datetime
+import time
+import os
+import json
+
+
+def read_codes(primary_key: str, second_key: str):
+    with open(os.path.dirname(__file__) + '/../input_files/codes.txt') as f:
+        code = json.loads(f.read())[primary_key][second_key]
+    return code
+
+
+def get_times():
+    today = datetime.date.today().strftime("%d/%m/%Y")
+    hour = time.strftime('%X %x %Z')[:5]
+    return today, hour
+
+
+def get_sense():
+    now = datetime.datetime.now()
+    if now < now.replace(hour=10, minute=0, second=0, microsecond=0):
+        direction = "1"
+    else:
+        direction = "2"
+    return direction
+
+
+def get_travel_df() -> pd.DataFrame:
+    current_day, current_time = get_times()
+    m = input("Input how you came 1) metro - bus, 2) metro - metro_ligero: ")
+    elapsed_time = int(input("Input your total time measured in minutes: "))
+    direction = read_codes("senses", get_sense())
+    data = pd.DataFrame({"date": current_day, "data_collection_time": current_time, "direction": direction,
+                         "means_of_transport": read_codes("means", m), "elapsed_time (mins)": elapsed_time}, index=[0])
+    return data
+
+
+def get_input_file(df: pd.DataFrame):
+    input_output = os.path.dirname(__file__) + '/../input_files'
+    files = os.listdir(input_output)
+    csv = [file for file in files if '.csv' in file]
+    # print(files, csv)
+    if not csv:
+        df.to_csv(input_output + "/historic_travels.csv", sep="|", index=False, header=True)
+        # print("No input DF, creating it")
+    else:
+        historic = pd.read_csv(input_output + "/historic_travels.csv", sep="|")
+        df = pd.concat([historic, df], ignore_index=True)
+        df.to_csv(input_output + "/historic_travels.csv", sep="|", index=False, header=True)
