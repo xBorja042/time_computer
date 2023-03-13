0606f51c834a8f2fc815e77db79dba20888d049e
bgonzalez <bgonzalez@vivelibre.es>
2023-03-13 13:11:45 +0100

    added changes to file and analytics file

---
+++ analytics/analytics.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+print("This is a script to analyze gathered data")
+
+print(os.getcwd(), os.listdir(), os.listdir("./../input_files"))
+
+file = 'historic_travels2.csv'
+input_file_path = os.path.join("./../input_files", file)
+
+df = pd.read_csv(filepath_or_buffer=input_file_path, sep="|")
+df["observation"] = ""
+
+print(df)
+
+output_file = "processed_file.csv"
+output_file_path = os.path.join("./../input_files", output_file)
+df.to_csv(output_file_path, sep="|")
+
+print(df)
+
+import matplotlib.pyplot as plt
+
+elapsed_times = df["elapsed_time (mins)"].to_numpy(copy=True)
+plt.scatter(range(len(elapsed_times)), elapsed_times)
+plt.title("Elapsed travel times measured in minutes")
+plt.show()
