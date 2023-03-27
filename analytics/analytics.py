0e38a5780bd0fa68c1762b53659e0eacad0f3968
bgonzalez <bgonzalez@vivelibre.es>
2023-03-27 12:29:49 +0200

    new changes to analytics file

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -11,23 +11,24 @@ input_file_path = os.path.join("./../input_files", file)
 df = pd.read_csv(filepath_or_buffer=input_file_path, sep="|")
 df["observation"] = ""
 
-print(df)
+# print(df)
 
 output_file = "processed_file.csv"
 output_file_path = os.path.join("./../input_files", output_file)
 df.to_csv(output_file_path, sep="|")
 
-print(df)
+# print(df)
 
 import matplotlib.pyplot as plt
 
-elapsed_times = df["elapsed_time (mins)"].to_numpy(copy=True)
-plt.scatter(range(len(elapsed_times)), elapsed_times)
-df["elapsed_time (mins)"].plot()
-plt.title("Elapsed travel times measured in minutes")
-plt.savefig("output_files/times_plot.png")
+for direction in df["direction"].unique().tolist():
+    print(" DIRECTION ", direction)
+    sub_df = df.loc[df["direction"] == direction].reset_index()
+    elapsed_times = sub_df["elapsed_time (mins)"].to_numpy(copy=True)
+    plt.scatter(range(len(elapsed_times)), elapsed_times)
+    print(sub_df)
+    sub_df["elapsed_time (mins)"].plot()
+    plt.title("Elapsed travel times measured in minutes")
+    # plt.savefig("output_files/times_plot.png")
+plt.legend(["Ida", "", "Vuelta", ""])
 plt.show()
-
-
-
-# plt.show()
