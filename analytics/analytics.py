b409e1a9382f6b4e2ec36789d7640a273c7771df
bgonzalez <bgonzalez@vivelibre.es>
2023-04-03 10:45:59 +0200

    changes to analytics plot

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -4,7 +4,7 @@ import os
 
 print("This is a script to analyze gathered data")
 
-print(os.getcwd(), os.listdir(), os.listdir("./../input_files"))
+# print(os.getcwd(), os.listdir(), os.listdir("./../input_files"))
 
 file = 'historic_travels2.csv'
 input_file_path = os.path.join("./../input_files", file)
@@ -22,17 +22,22 @@ df.to_csv(output_file_path, sep="|")
 
 import matplotlib.pyplot as plt
 
+q = 12
+
 for direction in df["direction"].unique().tolist():
     print(" DIRECTION ", direction)
     sub_df = df.loc[df["direction"] == direction].reset_index()
     elapsed_times = sub_df["elapsed_time (mins)"].to_numpy(copy=True)
-    plt.scatter(range(len(elapsed_times)), elapsed_times)
-    plt.plot(range(len(elapsed_times)), [np.mean(elapsed_times) for i in range(len(elapsed_times))])
-    print(sub_df)
-    sub_df["elapsed_time (mins)"].plot()
+    mean_time = np.mean(elapsed_times)
+    plt.plot(range(len(elapsed_times)), elapsed_times, marker="o")
+    plt.plot(range(len(elapsed_times)), [mean_time for i in range(len(elapsed_times))])
+    plt.text(q, 70, direction + "_" + str(mean_time)[:2], rotation=40, verticalalignment='center')
+    q += 12
     plt.title("Elapsed travel times measured in minutes")
+    plt.legend([direction, "Media_" + direction])
+    plt.savefig("output_files/times_plot" + direction + ".png")
 
-    plt.legend(["Ida", "", "Vuelta", ""])
-plt.savefig("output_files/times_plot.png")
+plt.legend(["Ida", "tiempo_medio_ida", "vuelta", "tiempo_medio_vuelta"])
+plt.savefig("output_files/times_plot" + direction + ".png")
 plt.show()
 
