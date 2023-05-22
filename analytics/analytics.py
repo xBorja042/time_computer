d3cbed38916f3933efd145c1704a97582fe2d8e5
bgonzalez <bgonzalez@vivelibre.es>
2023-05-22 09:03:36 +0200

    closed day development

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -4,21 +4,19 @@ import os
 
 print("This is a script to analyze gathered data")
 
-# print(os.getcwd(), os.listdir(), os.listdir("./../input_files"))
 
-file = 'historic_travels2.csv'
+file = 'historic_travels4.csv'
 input_file_path = os.path.join("./../input_files", file)
 
 df = pd.read_csv(filepath_or_buffer=input_file_path, sep="|")
 df["observation"] = ""
 
-# print(df)
 
 output_file = "processed_file.csv"
 output_file_path = os.path.join("./../input_files", output_file)
 df.to_csv(output_file_path, sep="|")
 
-# print(df)
+
 
 import matplotlib.pyplot as plt
 
